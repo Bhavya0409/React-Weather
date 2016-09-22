b8c889734160b565d40b2a1e54f7a5bc873a5be7
Bhavya Shah <Bhavya Shah@MCKINSEY.COM>
2016-09-22 13:15:20 -0400

    centered text and formatted buttons

--- app/components/Weather.jsx
+++ app/components/Weather.jsx
@@ -25,17 +25,13 @@ var Weather = React.createClass({
     that.setState({isLoading: false});
       alert(errorMessage);
     });
-  //   this.setState({
-  //     location: location,
-  //     temp: 23
-  //   });
   },
   render: function(){
     var {isLoading, temp, location} = this.state;
 
     function renderMessage(){
       if (isLoading){
-        return <h3>Fetching Weather...</h3>;
+        return <h3 className="text-center">Fetching Weather...</h3>;
       } else if (temp && location) {
         return <WeatherMessage location={location} temp={temp}/>
       }
@@ -43,6 +39,7 @@ var Weather = React.createClass({
 
     return(
       <div>
+      <h3 className="text-center">Get Weather</h3>
         <WeatherForm onSearch={this.handleSearch}/>
         {renderMessage()}
       </div>
